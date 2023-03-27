9f260ead516850fb6d9c7b2bbea84cd791c5c3ef
Crisp-Casper <88099085+Crisp-Casper@users.noreply.github.com>
2023-03-26 21:40:27 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-pkg install rust
+pkg install rust -y
 cargo install genact
 export PATH=$PATH:~/.cargo/bin/
 curl https://raw.githubusercontent.com/Crisp-Casper/Genact-for-Termux/main/bash.bashrc >${PREFIX}/etc/bash.bashrc
